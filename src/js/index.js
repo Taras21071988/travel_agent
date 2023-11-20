8c590db2f95652ad4a090b46a7092ce3b33deda5
Taras21071988 <tsasojlov@icloud.com>
2023-11-20 11:02:31 +0200

    cleaning files

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,3 @@
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
 import mobileNav from "./modules/mobile-nav.js";
-new AirDatepicker("#date");
 
 mobileNav();
