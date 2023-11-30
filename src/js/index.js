8e1ad7af762af3a616362995b5daa5578960933a
Taras21071988 <tsasojlov@icloud.com>
2023-11-30 12:07:15 +0200

    moving autocomplete to a separate file

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,6 @@
-import autoComplete from "@tarekraafat/autocomplete.js";
-import countryList from "./helpers/countryList";
+import autoCompleteFunc from "./modules/autoComplete";
 
-const autoCompleteJS = new autoComplete({
-  selector: "#autoCompleteCountry",
-  placeHolder: "e.g Bali, Indonesia",
-  data: {
-    src: countryList,
-    cache: true,
-  },
-  resultItem: {
-    highlight: true,
-  },
-  events: {
-    input: {
-      selection: (event) => {
-        const selection = event.detail.selection.value;
-        autoCompleteJS.input.value = selection;
-      },
-    },
-  },
-  resultList: {
-    maxResults: 10,
-  },
-  searchEngine: "strict",
-});
-
+autoCompleteFunc();
 //
 // import mobileNav from "./modules/mobile-nav.js";
 
