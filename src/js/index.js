ec5e22ea72c11586fc3cb377191717e20b2703bd
Taras21071988 <tsasojlov@icloud.com>
2023-11-30 13:37:16 +0200

    connection easepicker date

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 import autoCompleteFunc from "./modules/autoComplete";
+import { easepick } from "@easepick/bundle";
+
+
+const picker = new easepick.create({
+  element: document.getElementById("datepicker"),
+  css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
+});
 
 autoCompleteFunc();
 //
