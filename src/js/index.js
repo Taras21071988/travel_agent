9a4ca62da42417f1e19a6d99db1ea7681d350b1c
Taras21071988 <tsasojlov@icloud.com>
2023-11-30 14:08:30 +0200

    setting TimePlugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 import autoCompleteFunc from "./modules/autoComplete";
 import { easepick } from "@easepick/bundle";
-
+import { TimePlugin } from "@easepick/time-plugin";
 
 const picker = new easepick.create({
-  element: document.getElementById("datepicker"),
+  element: "#datepicker",
   css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
+  zIndex: 10,
+  format: "HH:mm, DD/MM/YY",
+  plugins: [TimePlugin],
+  TimePlugin: {
+    format: "HH:mm",
+  },
 });
-
 autoCompleteFunc();
 //
 // import mobileNav from "./modules/mobile-nav.js";
