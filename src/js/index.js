3b5dcfe1cb136d69f8c11888db24233b5fa63247
Taras21071988 <tsasojlov@icloud.com>
2023-12-05 12:55:09 +0200

    connect swiper in js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,22 @@
 import autoCompleteFunc from "./modules/autoComplete";
 import picker from "./modules/picker";
-import Swiper from 'swiper';
-import 'swiper/css';
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: "#sliderNext",
+    prevEl: "#sliderPrev",
+  },
+});
 
 autoCompleteFunc();
 picker;
 
-// const swiper = new Swiper(...);
-
-//
 // import mobileNav from "./modules/mobile-nav.js";
 
 // mobileNav();
-
-
