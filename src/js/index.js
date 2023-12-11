62b211c32c7222abbe3079d80d6ddb9c86bf2a74
Taras21071988 <tsasojlov@icloud.com>
2023-12-11 11:35:40 +0200

    slight change to swiperjs and styles

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper", {
   loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   slidesPerView: 4,
   spaceBetween: 32,
   navigation: {
