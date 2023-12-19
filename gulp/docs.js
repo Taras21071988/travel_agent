3cd1cef731e90e663ceb9a32d7b75ad350c7dbb0
Taras21071988 <tsasojlov@icloud.com>
2023-12-19 12:05:07 +0200

    changing gulp settings for a more correct build of the project

--- gulp/docs.js
+++ gulp/docs.js
@@ -50,7 +50,7 @@ const plumberNotify = (title) => {
 
 gulp.task("html:docs", function () {
   return gulp
-    .src(["./src/html/**/*.html", "!./src/html/blocks/*.html"])
+    .src(["./src/html/**/*.html", "!./src/html/blocks/**/*.html"])
     .pipe(changed("./docs/"))
     .pipe(plumber(plumberNotify("html")))
     .pipe(fileInclude(fileIncludeSetting))
