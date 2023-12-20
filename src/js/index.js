dcbece7a734cd8d7a5e8aaba14ddd760370ce23a
Taras21071988 <tsasojlov@icloud.com>
2023-12-20 15:56:01 +0200

    burger layout

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,11 @@ import picker from "./modules/picker";
 import reveal from "./modules/scrollreveal.js";
 import swiper from "./modules/swiper.js";
 import swiperTestim from "./modules/swiper-testimonials.js";
+import mobileNav from "./modules/mobile-nav.js";
 
+mobileNav();
 autoCompleteFunc();
 picker;
 reveal;
 swiper;
 swiperTestim();
-// import mobileNav from "./modules/mobile-nav.js";
-
-// mobileNav();
